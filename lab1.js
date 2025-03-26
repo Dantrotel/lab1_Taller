e3e1d45b37b0f4f80eb1790e279b477e87606d82
Gonifeck-Dev <gonzalo.fclopez@gmail.com>
2025-03-26 12:21:57 -0300

    queso

--- lab1.js
+++ lab1.js
@@ -18,3 +18,5 @@ gameOfLife.insertCelula(Matriz,1,3);
 gameOfLife.imprimirMatriz(Matriz);
 
 gameOfLife.iniciarJuego(Matriz,1000);
+
+console.log("JUEGO DE LA VIDA SEGUNDA PARTE: EL REGRESO DE ARAYA");
